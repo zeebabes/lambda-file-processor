32875024dc6321f44ccfccab1d71a035883ebc5d
KemiA <kzagbabiaka@gmail.com>
2025-05-29 16:27:49 -0400

    initial

--- main.tf
+++ main.tf
@@ -57,3 +57,36 @@ resource "aws_s3_bucket_notification" "lambda_trigger" {
 
   depends_on = [aws_lambda_permission.allow_s3]
 }
+
+resource "aws_apigatewayv2_api" "lambda_api" {
+  name          = "lambda-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id           = aws_apigatewayv2_api.lambda_api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = aws_lambda_function.s3_trigger_lambda.invoke_arn
+  integration_method = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "default_route" {
+  api_id    = aws_apigatewayv2_api.lambda_api.id
+  route_key = "ANY /"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.lambda_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "apigw_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.s3_trigger_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.lambda_api.execution_arn}/*/*"
+}
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "s3_bucket_name" {
 output "lambda_function_name" {
   value = aws_lambda_function.s3_trigger_lambda.function_name
 }
+
+output "api_gateway_url" {
+  value = aws_apigatewayv2_api.lambda_api.api_endpoint
+}
